6284226b19fe8657cfc98e3ffc28cec5f48dd050
Pavibas <pavibas05@gmail.com>
2023-10-05 10:15:37 +0700

    Task 4: Upload the Docker Image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="pavithradevithambusamy/operationalize-a-machine-learning-microservice-api:latest"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker logout
+docker login -u pavithradevithambusamy
+docker tag learning-microservice-api $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
